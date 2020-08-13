03407570f13d1db0d7137948e81e43bf1a20dc16
ireneo-cobarjr <ireneo.cobar.jr@gmail.com>
2020-08-14 01:16:05 +0800

    Added Comments on layout files of customer and brand pages.

--- src/layouts/BrandLayout.vue
+++ src/layouts/BrandLayout.vue
@@ -66,11 +66,24 @@ export default {
   name: 'MainLayout',
   data () {
     return {
+      //* This property just controls the state of the search box, search button and toolbar button.
+      //* If filters dropdown is visible, the other controls are disabled until the filters dropdown is hidden back.
+      //* So as to not make users make unlikely activities at the same time like adding a search term on the search box while the filters is still visible.
       controlState: false,
+
+      //* This property just sets the current page's title on the toolbar
       title: '',
+
+      //* This property is the model for the search box and holds the value for any text input on the search box.
       searchTerm: '',
+
+      //* This property is used to save the latest search term to be used on the reset method.
       searched: '',
+
+      //* The final result of search and filters applied. This is the data viewed by all users at all times.
       filteredResults: [],
+
+      //* Data used by all filters. factory functions that build filters also store its result here.
       filters: {
         categories: [],
         delivery_dow: [],
@@ -79,22 +92,37 @@ export default {
         created_at: { min: '', max: '' },
         active: { min: '', max: '' }
       },
+
+      //* This proprty stores the base state of date based filters. To be used by reset method.
       dateBaseState: {
         created_at: { min: '', max: '' },
         active: { min: '', max: '' }
       },
+
+      //* Used by all filters. For each SKU who met a filters condition will have its sku_id placed at this array.
+      //* This array holds the final sku id after applying filters and search term.
       ticks: [],
+
+      //* Holds the state of the spinner component used by the loading scene.
       loading: true,
+
+      //* This holds the state of productView page in relevance to navigation.
       isSingleProduct: false
     }
   },
   computed: {
+    //* Precalculates the seach result. If the search term is blank, then this returns all sku from the endpoint.
+    //* This property is not what users sees, but is rather the basis for the final result.
     searchResults () {
       return this.$store.getters['brand/searchProducts'](this.searchTerm)
     },
+
+    //* Process categories to be used by category filters.
     categories () {
       return this.$store.getters['brand/getCategories']
     },
+
+    //* Saves the base state of the ticks property. To be used by the reset method.
     baseTicks () {
       const ticks = []
       this.searchResults.forEach(sku => { ticks.push(sku.id) })
@@ -102,6 +130,16 @@ export default {
     }
   },
   methods: {
+    //* /////////////////////////////////////////
+    //* Factory Functions.
+    //* /////////////////////////////////////////
+    //* ///
+    // ? Each Build Filter Functions process filters as arrays with objects having a property of label and id
+    // ? so as to be compatible with Quasar's QTree component.
+
+    //* Builds filters that uses dates from properties on the root of searchResults/sku. This is a reusable function.
+    //* Parameter: property is a string that identifies the property with a date string value to be used as a filter.
+    //* Return value: None. Directly saves data to filters property and dateBaseState property.
     buildDateFilters (property) {
       const set = []
       this.searchResults.forEach(sr => {
@@ -111,9 +149,14 @@ export default {
       this.filters[property].max = this.getMaxDate(set)
       this.dateBaseState[property] = this.filters[property]
     },
+
+    //* Builds filters from each sku's prices array property. This is a reusable function.
+    //* Parameter: property is a string that identifies each sku's prices array to be used as a filter.
+    //* Return value: None. Directly saves data to filters property.
     buildPriceFilter (property) {
       this.filters[property] = []
-      //* Not all sku objects have a valid price array. Gather only those that are valid
+      //* Not all sku objects have a valid price array. Gather only those that are valid.
+      //* filter searchResults and save to sku all skus with a none empty prices array.
       const skus = this.searchResults.filter(sku => sku.prices.length)
       if (skus.length) {
         skus.forEach(sku => {
@@ -148,6 +191,9 @@ export default {
         pf.label = `${pf.label} (${pf.children.length})`
       })
     },
+
+    //* Creates the filters for the categories based on the searchResults array.
+    //* Return value: None. Directly saves data to filters property.
     buildCategoryFilters () {
       const a = this.categories.filter(category => this.searchResults.some(s => s.category.parent_id === category.id))
       this.filters.categories = []
@@ -167,6 +213,8 @@ export default {
         })
       })
     },
+
+    //* Consolidates all filter factory functions.
     createFilters () {
       this.buildCategoryFilters()
       this.buildPriceFilter('delivery_dow')
@@ -175,26 +223,59 @@ export default {
       this.buildDateFilters('created_at')
       this.buildDateFilters('active')
     },
+
+    // ? All methods that starts with 'on' like 'onSearch' are methods that responds to certain events.
+
+    //* Responds to events creationDateInput and activeDateInput from the router-view components, specifically, QDate component.
+    //* This method is responsible for making sure that date based filters will register sku_ids that meets its conditions on the ticks property.
     onDateFiltered (x) {
       const { property, arg } = x
       this.filters[property] = arg
       this.filterByDate(this.searchResults, property, this.filters[property])
     },
+
+    //* Resets all filters and result back to its former state before any changes to any filters were applied.
+    //* Responds to the 'reset' event from components of router-view.
+    //* This method fires when the 'Reset' Button is clicked from the filters dropdown menu.
     onReset () {
       this.ticks = this.baseTicks
       this.filters.active = this.dateBaseState.active
       this.filters.created_at = this.dateBaseState.created_at
       this.filteredResults = this.$store.getters['brand/searchProducts'](this.searched)
     },
+
+    //* Applies filter to searchReuslts array.
+    //* Basically, it checks ticks array for all sku_ids on it, and uses it to filter the searchResult.
+    //* Directly saves the result to filteredResults array and is instantly visible to the user.
+    //* This method fires when the 'Apply' button from the filters dropdown menu is clicked.
     onFiltered () {
       this.filteredResults = this.searchResults.filter(result => this.ticks.some(tickID => tickID === result.id))
     },
+
+    // ? NOTES:
+    // ? Remember that changing the searchResult by changing the searchTerm on the searchBox and clicking the seach button will also change the filteredResults.
+    // ? Changing filteredResults by changing values of filters from the filters dropdown menu and clicking the apply button will never change or affect searchResult.
+
+    //* Fires when the search button from the toolbar is clicked.
+    //* Take note that searchResults is already pre-calculated since its already a computed property and is ONLY A BASIS for the final filteredResults
     onSearch () {
+      // saves the typed in search term from the search box to searched property so that we may be able to reset it later if needed.
       this.searched = this.searchTerm
+      // saves the searchResults array value to the filteredResults.
       this.filteredResults = this.searchResults
+      // gets the baseTicks and gets its value to ticks property, remember that baseTicks is also a computed property based on searchResults
       this.ticks = this.baseTicks
+      // creates filters based on the searchResults.
       this.createFilters()
     },
+
+    //* /////////////////////////////////////////
+    //* These are helper fumctioms
+    //* /////////////////////////////////////////
+
+    //* This function is used by buildDateFilters() method. This function finds the newest date from src array
+    //* Parameters: src is an array of strings of dates.
+    //* Return value: a date in string format (e.g. /2020/8/10) that can readily be consumed by Quasar's QDate component.
     getMaxDate (src) {
       let max = src[0]
       for (let x = 1; x < (src.length - 1); x++) {
@@ -209,6 +290,10 @@ export default {
       const date = new Date(max)
       return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
     },
+
+    //* This function is used by buildDateFilters() method. This function finds the oldest date from src array
+    //* Parameters: src is an array of strings of dates.
+    //* Return value: a date in string format (e.g. /2020/8/10) that can readily be consumed by Quasar's QDate component.
     getMinDate (src) {
       let min = src[0]
       for (let x = 1; x < (src.length - 1); x++) {
@@ -223,6 +308,10 @@ export default {
       const date = new Date(min)
       return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()}`
     },
+
+    //* This function is used by onDateFiltered() method. This function directly filters src using the passed filtered parameter and saves all sku_id
+    //* of filtered src to ticks array. This function doesnt direclty change the result users see but instead prepapares the filter process.
+    //* Parameters: src is the array that will be filtered, property is the property name to be used as a filter, and filter is the filter value as an object.
     filterByDate (src, property, filter) {
       let test = []
       if (filter.min !== '') {
@@ -238,6 +327,8 @@ export default {
       test.forEach(t => { this.ticks.push(t.id) })
       this.$root.$emit('dateFiltered', this.ticks)
     },
+
+    //* Responds to setTitle event fired from the router-view component and set the page title on the toolbar.
     onNav (e) {
       this.title = e
       this.searchTerm = ''
@@ -248,11 +339,23 @@ export default {
     }
   },
   async created () {
+    // * Functions here will always fire whenever users navigate to this page.
+    //* Sets loading screen on while all functions at vue created lifecycle hook are not yet finished running.
     this.loading = true
+
+    //* Fetch data from the server using the endpoints defined at /boot/axios and store it on Vuex state.
     await this.$store.dispatch('brand/getProducts')
     await this.$store.dispatch('brand/getCategories')
+
+    //* Runs onSearch from the first time. This will initialize most properties like filters.
     this.onSearch()
+
+    //* Stops the loading screen
     this.loading = false
+
+    //* Global Bus Event
+    //* Responds to ticked event from filter components. This ensures that all filters are synced.
+    //* arg is the value received from the event. All filters sends their ticks via this receiver.
     this.$root.$on('ticked', (arg) => { this.ticks = arg })
   }
 }
